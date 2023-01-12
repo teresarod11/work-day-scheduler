5032cac1486ffcef83db3a544629c663d631e601
Teresa Rodriguez <teresarodriguez@Teresas-iMac.local>
2023-01-11 19:19:01 -0500

    updated the readme file

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 ## Description
 
-A simple calendar app for scheduling your day.   
+This is a simple calender app for scheduling you work day using dayjs and jquery. The time and day is display at the top of the page for the user. The calender consists of time block from 9am-5pm that are color-coded depending on the time of day. These colors represent the past(grey), present(red) and future(green). The user is able to save their input by clicking the save button on the right of the time block. Perfect for scheduling your day to day chores/work.
 
-## First Question
-![screenshot](assets/images/first.image.png)
+## Full page view
+![screenshot](assets/images/layout.png)
 
+## Saved in localstorage
+![screenshot](assets/images/saved.png)
 
 ## Link    
 
-https://github.com/teresarod11/Password-Generator.git
+https://teresarod11.github.io/work-day-scheduler/
--- assets/js/style.js
+++ assets/js/style.js
@@ -17,13 +17,13 @@ var btnSave = $('.saveBtn');
 
 // function that wraps around everything 
 $(function(){ 
-
+// this function helps display the time onto the calendar
     function displayTime() {
         var currentDay = $('#currentDay'); 
         var rightNow = dayjs().format('MMMM DD, YYYY [at] hh:mm A');
         currentDay.text(rightNow);
       }
-   
+    //this click function lets the user click on the save button and targets the users input to set in localstorage.
     btnSave.on("click", function (event) {
         event.preventDefault();
         var value = $(this).siblings('.description').val();
@@ -32,7 +32,7 @@ $(function(){
        localStorage.setItem(time, value);
        console.log(event);
     });
-
+    // this makes sure that each text area block is stored into the local storage
     $("#hour-09 .description").val(localStorage.getItem("hour-09"));
     $("#hour-10 .description").val(localStorage.getItem("hour-10"));
     $("#hour-11 .description").val(localStorage.getItem("hour-11"));
@@ -42,9 +42,9 @@ $(function(){
     $("#hour-15 .description").val(localStorage.getItem("hour-15"));
     $("#hour-16 .description").val(localStorage.getItem("hour-16"));
     $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-  
+//   this makes sure to target each time block and loops through the color-coding from past(grey), present(red), and future (green).
    $('.time-block').each(function(){
-    var block = $(this).attr('id').split('-')[1];
+    var block = $(this).attr('id').split('-')[1]; //this targets only the number element
     // console.log(block);
     var time = dayjs().format('HH');
     // console.log(time);
